ce278ad3056c31a3e0147cf55f0c64d98c1f2f21
Vangnex <chengdidier@gmail.com>
2024-06-10 19:32:02 +0200

    UPDATE Questionnaire number + ADD results and freelances pages

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Survey from './pages/Survey';
+import Results from './pages/Results';
+import Freelances from './pages/Freelances';
 
 import Header from './components/Header';
 import Error from './components/Error';
@@ -16,6 +18,8 @@ root.render(
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/survey/:questionNumber" element={<Survey />} />
+                <Route path="/freelances" element={<Freelances />} />
+                <Route path="/results" element={<Results />} />
                 <Route path="*" element={<Error />} />
             </Routes>
         </Router>
--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -1,12 +1,21 @@
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 
 function Survey() {
     const { questionNumber } = useParams();
+    const questionNumInt = parseInt(questionNumber);
+    const prevQuestionNum = questionNumInt === 1 ? 1 : questionNumInt - 1;
+    const nextQuestionNum = questionNumInt + 1;
 
     return (
         <div>
             <h1>Questionnaire 🧮</h1>
             <h2>Question {questionNumber}</h2>
+            <Link to={`/survey/${prevQuestionNum}`}>Précédent</Link>
+            {questionNumInt === 10 ? (
+                <Link to="/results">Résultats</Link>
+            ) : (
+                <Link to={`/survey/${nextQuestionNum}`}>Suivant</Link>
+            )}
         </div>
     );
 }
