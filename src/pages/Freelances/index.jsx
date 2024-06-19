e084a14bc44a1b48c746ce7e48aa11381f7464e4
Vangnex <chengdidier@gmail.com>
2024-06-19 20:31:30 +0200

    ADD Card components and usage of prop-types in it for test

--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,7 +1,32 @@
+import Card from '../../components/Card';
+
+const freelanceProfiles = [
+    {
+        name: 'Jane Doe',
+        jobTitle: 'Devops',
+    },
+    {
+        name: 'John Doe',
+        jobTitle: 'Developpeur frontend',
+    },
+    {
+        name: 'Jeanne Biche',
+        jobTitle: 'Développeuse Fullstack',
+    },
+];
+
 function Freelances() {
     return (
         <div>
-            <h1>Freelances 👩‍💻</h1>
+            <h1>Freelances 👩‍💻👨‍💻</h1>
+            {freelanceProfiles.map((profile, index) => (
+                <Card
+                    key={`${profile.name}-${index}`}
+                    label={profile.jobTitle}
+                    picture={profile.picture}
+                    title={profile.name}
+                />
+            ))}
         </div>
     );
 }
